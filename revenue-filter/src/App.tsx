185960801c0d32da49a03a9367ccab04529f995a
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-11-27 07:17:34 +0100

    feat: balance lists

--- revenue-filter/src/App.tsx
+++ revenue-filter/src/App.tsx
@@ -4,34 +4,16 @@ import SideBar from "./components/SideBar";
 import NavBar from "./components/NavBar";
 
 import "./App.css";
+import Balance from "./components/Balance";
 
 function App() {
-  const [balance, setBalance] = useState("");
-
-  useEffect(() => {
-    async function getBalance() {
-      try {
-        const response = await apiClient.get("/wallet");
-        setBalance(response.data.balance);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getBalance();
-  });
-
   return (
     <section className="font-Degular h-full text-black flex relative">
       <SideBar />
 
       <div className="flex flex-col flex-1">
         <NavBar />
-        <div className="p-8">
-          <p className="text-xl font-DegularThin">Available Balance</p>
-          <h1></h1>
-          <button>Withdraw</button>
-          {balance ? <h2>USD {balance} </h2> : <p> loading</p>}
-        </div>
+        <Balance />
       </div>
     </section>
   );
--- revenue-filter/src/components/Balance.tsx
+++ revenue-filter/src/components/Balance.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+import info from "../assets/icons/info.svg";
+import { useEffect, useState } from "react";
+import { apiClient } from "../core/api";
+
+const BalanceList: React.FC<{ balancesData: BalanceItem[] }> = ({
+  balancesData,
+}) => {
+  return (
+    <div className="w-1/2 ">
+      {balancesData.map((balance, id) => (
+        <div key={id} className="flex items-center justify-between border p-4">
+          <div>
+            <p className="text-lg font-semibold">{balance.title}</p>
+            <p className="text-xl font-bold">USD {balance.amount}</p>
+          </div>
+          <img src={info} alt={`info-icon-${id}`} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Balance = () => {
-  return <div>Balance</div>;
+  const [balancesData, setBalancesData] = useState<BalanceItem[]>([]);
+  const [balance, setBalance] = useState("");
+
+  useEffect(() => {
+    async function getBalance() {
+      try {
+        const response = await apiClient.get("/wallet");
+        const responseData = response.data;
+
+        // Mapping response data to BalanceItem structure
+        const mappedBalances: BalanceItem[] = Object.keys(responseData).map(
+          (key) => ({
+            title: key,
+            amount: responseData[key],
+          })
+        );
+
+        setBalancesData(mappedBalances);
+        setBalance(response.data.balance);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getBalance();
+  });
+
+  return (
+    <section className="flex h-[400px]">
+      <div className="p-8 border w-1/2 h-1/2 relative flex flex-col items-start">
+        <p className="text-sm font-DegularThin">Available Balance</p>
+        <button className="absolute bg-black rounded-full px-8 py-3 text-xs text-white">
+          Withdra
+        </button>
+        {balance ? (
+          <h2 className="DegularBold text-2xl font-extrabold">
+            {" "}
+            USD {balance}{" "}
+          </h2>
+        ) : (
+          <p> loading</p>
+        )}
+      </div>
+      <BalanceList balancesData={balancesData} />
+    </section>
+  );
+};
+
+type BalanceItem = {
+  title: string;
+  amount: number;
 };
 
 export default Balance;
